10fc9937e3176fb1e5eda4313bdc610345ad4c55
Atakan Suslu <atakan.suslu@metu.edu.tr>
2022-12-17 21:44:33 +0300

    Waypoints from camera

--- parameters.m
+++ parameters.m
@@ -1,7 +1,9 @@
 
+% Unit conversion
+rad = 180/pi;
+deg = pi/180;
 
 % Color Mask Parameters
-
 R_min = uint8(127);
 R_max = uint8(255);
 
@@ -10,3 +12,11 @@ G_max = uint8(151);
 
 B_min = uint8(0);
 B_max = uint8(149);
+
+% Line Search parameters
+searchAngle = 160; % degrees
+searchRange = 59; % pixels
+initialSearchAngle = 90; % degrees
+
+% Takeoff wait
+takeoffWaitTime = 1.2; % seconds
